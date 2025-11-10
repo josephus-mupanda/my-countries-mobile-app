0fd2e2642147fb4a7b76869c2b653181581038f6
josephus <josephusmupanda48@gmail.com>
2025-11-10 12:04:57 +0200

    app config

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class AppConfig {
+  
+  static String get apiBaseUrl => dotenv.env['API_BASE_URL'] ?? 'https://restcountries.com/v3.1/';
+  static const Duration cacheDuration = Duration(days: 7);
+  static const int searchDebounceMs = 400;
+
+}
