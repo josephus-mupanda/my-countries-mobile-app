eb61606e1521dc6a89dd1fe591fc8929cc930819
josephus <josephusmupanda48@gmail.com>
2025-11-11 16:01:58 +0200

    feat(home): modify home page and app router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,5 +1,8 @@
+import 'package:countries_app/core/di/service_locator.dart';
+import 'package:countries_app/logic/country_details/country_details_bloc.dart';
 import 'package:countries_app/presentation/pages/not_found_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../presentation/pages/home_page.dart';
 import '../../presentation/pages/country_detail_page.dart';
 import '../../presentation/pages/favorites_page.dart';
@@ -7,20 +10,29 @@ import 'routes.dart';
 
 class AppRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
-    
     switch (settings.name) {
+      
       case AppRoutes.home:
         return MaterialPageRoute(builder: (_) => const HomePage());
+
       case AppRoutes.detail:
-        final code = settings.arguments as String;
-        return MaterialPageRoute(builder: (_) => CountryDetailPage(cca2: code));
+        final args = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (_) => sl<CountryDetailsBloc>(),
+            child: CountryDetailPage(
+              cca2: args['cca2'],
+              flagUrl: args['flagUrl'],
+              name: args['name'],
+            ),
+          ),
+        );
+
       case AppRoutes.favorites:
         return MaterialPageRoute(builder: (_) => const FavoritesPage());
-      
+
       default:
-        return MaterialPageRoute(
-          builder: (_) => const NotFoundScreen ()
-        );
+        return MaterialPageRoute(builder: (_) => const NotFoundScreen());
     }
   }
 }
